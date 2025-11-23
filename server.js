216ae23a92e5f268d541b6fce81bd9f580c34044
chauhaidang1307 <chauhaidang06@gmail.com>
2025-11-23 18:10:56 +0700

    Gộp các nhánh vào branch để tiếp tục

--- server.js
+++ server.js
@@ -704,19 +704,3 @@ app.post("/notifications/send-all", (req, res) => {
 
   return res.json({ message: "Đã gửi tất cả thông báo thành công!" });
 });
-
-import express from 'express'
-const app = express();
-import cors from 'cors';
-import StudentsRouter from './src/Students/StudentRoute/StudentRoute.js'
-app.use(express.json())
-app.use(cors());
-app.use(express.urlencoded({ extended: true }))
-
-app.use("/student",StudentsRouter);
-
-
-
-app.listen(8080,() =>{
-    console.log("Server started on port 8080");
-})
